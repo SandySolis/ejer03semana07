eaac61268e91de055afe052e5fb6a8f89d257753
Sandy Solis <sandysolis0305@gmail.com>
2023-10-03 21:49:49 -0500

    Agregar archivos de proyecto.

---
+++ ejer03semana07/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejer03semana07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int año1 = 1900;
+            int año2 = 2023; 
+
+            Console.WriteLine("Años bisiestos desde " + año1 + " hasta " + año2 + ":");
+
+            for (int i = año1; i <= año2; i++)
+            {
+                if ((i % 4 == 0 && i % 100 != 0) || (i % 400 == 0))
+                {
+                    Console.WriteLine("- " + i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
